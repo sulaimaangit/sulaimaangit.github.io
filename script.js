d15bc888f52d551e52e58782fe368152485f0b47
Sulaimaan <105475391+sulaimaangit@users.noreply.github.com>
2024-02-23 21:47:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student", "Developer","Home Cook","Designer"],
+        strings: ["Student", "Developer","Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Student", "Developer","Designer", "Home Cook"],
+        strings: ["Student", "Developer","Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
